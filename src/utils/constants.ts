3996c9264a43149862bf5d77f50996367ae1a638
hari <haridarshanofficial@gmail.com>
2022-11-19 11:18:19 +0530

    static designs done

---
+++ src/utils/constants.ts
@@ -0,0 +1,30 @@
+export const quizzes = [
+	{
+		questions: [
+			{
+				id: 1,
+				question: 'What is 2 + 2?',
+				answer: 3,
+				options: ['1', '2', '3', '4'],
+			},
+			{
+				id: 2,
+				question: 'What is 3 + 3?',
+				answer: 2,
+				options: ['4', '5', '6', '7'],
+			},
+			{
+				id: 3,
+				question: 'What is 4 + 4?',
+				answer: 2,
+				options: ['6', '7', '8', '9'],
+			},
+			{
+				id: 4,
+				question: 'What is 5 + 5?',
+				answer: 2,
+				options: ['8', '9', '10', '11'],
+			},
+		],
+	},
+];
